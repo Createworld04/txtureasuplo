cd9c718838209951e545317e19d26b62ec2135a7
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-09-26 14:41:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telethon import events, Button
+import telethon
 from config import bot
 import m3u8_To_MP4
 import logging
@@ -38,11 +39,15 @@ async def _(event):
             file_name = f"{name[1][:60]}.mkv"
             r = await event.reply(f"`Downloading...\n{name[1]}\n\nfile number: {name[0][:-1]}`")
             m3u8_To_MP4.download(i[1], mp4_file_name=file_name)
-            subprocess.call(f'''ffmpeg -i "{file_name}" -ss 00:00:00 -vframes 1 "thumbnal.jpg"''')
+            try:
+                os.remove("thumbnail.jpg")
+            except:
+                pass
+            subprocess.call(f'''ffmpeg -i "{file_name}" -ss 00:00:00 -vframes 1 "thumbnail.jpg"''')
             file = await fast_upload(bot, file_name, reply= r)
             await bot.send_message(event.chat_id, f"`{name[1]}\n\nfile number: {name[0][:-1]}`", file=file, force_document=False, thumb="thumbnail.jpg")
             os.remove(file_name)
-            os.remove("thumbnal.jpg")
+            os.remove("thumbnail.jpg")
             r.delete()
             break
     except Exception as e:
