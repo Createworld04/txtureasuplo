d3d9ddc8380374528b454790fa378983740b0471
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-09-29 11:40:19 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,19 +1,19 @@
 import json
-from os import waitpid
-
-
+from config import skeleton_url
+import subprocess
 
 def parse_json_to_txt(file_name):
     with open(file_name) as f:
         data = json.load(f)
     details = data["data"]["class_list"]["classes"]
     res = ""
+    batch_name = data["data"]["class_list"]["batchName"]
     for i in details:
         name = i["lessonName"]
         id = i["lessonUrl"][0]["link"]
-        link = f"https://thegreatace.herokuapp.com/{id}"
+        link = f"{skeleton_url}/{id}"
         res = f"{name}:{link}\n{res}"
-    new_file_name = file_name.replace(".json", ".txt")
+    new_file_name = f"{batch_name}.txt"
     with open(new_file_name, "w", encoding='utf-8') as f:
         f.write(res.strip())
     return new_file_name
@@ -62,7 +62,7 @@ def parse_json_to_html(file_name):
                 Maths Spl-28 (Ari+Adv.)
             </h1>
             <p id="info" style="text-align:center;font-size:25px;color:Blue">
-            Links grabber made by <a href="https://t.me/President_Shirogane" rel="noopener noreferrer" target="_blank">Miyuki (Add bot name later)</a>
+            Links grabber made by <a href="https://t.me/Portgas_D_ACEx" rel="noopener noreferrer" target="_blank">Scorpions Bot</a>
             <br>
             <br>
             </p>
@@ -73,7 +73,7 @@ def parse_json_to_html(file_name):
     batch_name = data["data"]["class_list"]["batchName"]
     head_syntax = head_syntax.replace("BATCH", batch_name)
     details = data["data"]["class_list"]["classes"]
-    with open(file_name.replace(".json", ".html"), "w") as f:
+    with open(f"{batch_name}.html", "w") as f:
         f.write(head_syntax)
     for i in range( len(details) - 1, -1, -1):
         body_syntax = '''
@@ -88,9 +88,15 @@ def parse_json_to_html(file_name):
         link = f"https://thegreatace.herokuapp.com/{id}"
         body_syntax = body_syntax.replace("NAME", name)
         body_syntax = body_syntax.replace("LINK", link)
-        with open(file_name.replace(".json", ".html"), "a", encoding='utf-8') as f:
+        with open(f"{batch_name}.html", "a", encoding='utf-8') as f:
             f.write(body_syntax)
-    return file_name.replace(".json", ".html")
+    return f"{batch_name}.html"
+    
 
-
-
+def duration(filename):
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    return float(result.stdout)
