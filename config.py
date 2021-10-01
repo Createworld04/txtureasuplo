e82771f270d0085549d3766e53960a79b592629c
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-10-01 11:54:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,9 @@ bot_token = os.environ.get('BOT_TOKEN')
 skeleton_url = os.environ.get('SKELETON_URL')
 auth_users = os.environ.get('AUTH_USERS').split()
 auth_groups = os.environ.get('AUTH_GROUPS').split()
+for i in range(len(auth_users)):
+    auth_users[i] = int(auth_users[i])
+for i in range(len(auth_groups)):
+    auth_groups[i] = int(auth_groups[i])
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
